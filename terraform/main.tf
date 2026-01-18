7e5c38d787b8439c329db80cf1370d85b8ec15ca
code-repo-samples <54771322+code-repo-samples@users.noreply.github.com>
2026-01-17 19:41:48 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,80 +10,5 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
-}
-
-# Package Lambda code into ZIP
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "../app/lambda_function.py"
-  output_path = "lambda.zip"
-}
-
-# IAM role for Lambda
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_rest_api_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-# Basic logging permissions
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# Lambda function
-resource "aws_lambda_function" "api_lambda" {
-  function_name = "sample-rest-api"
-  runtime       = "python3.11"
-  handler       = "lambda_function.lambda_handler"
-  role          = aws_iam_role.lambda_role.arn
-
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-}
-
-# HTTP API Gateway
-resource "aws_apigatewayv2_api" "http_api" {
-  name          = "sample-http-api"
-  protocol_type = "HTTP"
-}
-
-# Integration
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.http_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.api_lambda.invoke_arn
-  payload_format_version = "2.0"
-}
-
-# Route (catch-all)
-resource "aws_apigatewayv2_route" "api_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "ANY /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-# Stage
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
-}
-
-# Permission for API Gateway to invoke Lambda
-resource "aws_lambda_permission" "api_permission" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.api_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+  region = var.region
 }
