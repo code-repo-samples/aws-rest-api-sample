1a95a4ec4a67c5a852784b3de5405a0216ec01a2
code-repo-samples <54771322+code-repo-samples@users.noreply.github.com>
2026-01-17 19:35:39 -0500

    Create lambda.tf

---
+++ terraform/lambda.tf
@@ -0,0 +1,15 @@
+resource "aws_lambda_function" "api_lambda" {
+  function_name = "sample-rest-api-${var.environment}"
+  runtime       = "python3.11"
+  handler       = "lambda_function.lambda_handler"
+  role          = local.lambda_role_arn
+  filename      = data.archive_file.lambda_zip.output_path
+
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      ENVIRONMENT = var.environment
+    }
+  }
+}
