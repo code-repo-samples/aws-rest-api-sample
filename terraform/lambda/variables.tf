1b218028bb30b12dde8480cf851f597f598eaab3
code-repo-samples <54771322+code-repo-samples@users.noreply.github.com>
2026-01-17 20:05:32 -0500

    Update variables.tf

--- terraform/lambda/variables.tf
+++ terraform/lambda/variables.tf
@@ -1,15 +1,3 @@
-variable "environment" {
-  description = "Deployment environment (dev or staging)"
-  type        = string
-}
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "lambda_role_name" {
-  description = "IAM role name for Lambda"
-  type        = string
-}
+variable "environment" { type = string }
+variable "region" { default = "us-east-1" }
+variable "lambda_role_name" { type = string }
