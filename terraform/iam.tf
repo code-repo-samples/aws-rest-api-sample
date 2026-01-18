90de7cc4b120d7852a2423aab4591f41e3d23545
code-repo-samples <54771322+code-repo-samples@users.noreply.github.com>
2026-01-17 19:45:13 -0500

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,11 +1,7 @@
-# Try to read existing IAM role
 data "aws_iam_role" "existing_lambda_role" {
   name = var.lambda_role_name
-  # Ignore error if role does not exist
-  # Using Terraform >= 1.5, can wrap in try() later
 }
 
-# Create role only if it doesn't exist
 resource "aws_iam_role" "lambda_role" {
   count = can(data.aws_iam_role.existing_lambda_role.arn) ? 0 : 1
 
@@ -21,14 +17,12 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# Resolve ARN for Lambda regardless of whether created or reused
 locals {
-  lambda_role_arn = can(data.aws_iam_role.existing_lambda_role.arn)
-    ? data.aws_iam_role.existing_lambda_role.arn
-    : aws_iam_role.lambda_role[0].arn
+  lambda_role_arn = can(data.aws_iam_role.existing_lambda_role.arn) ? 
+    data.aws_iam_role.existing_lambda_role.arn : 
+    aws_iam_role.lambda_role[0].arn
 }
 
-# Attach basic Lambda execution policy
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = local.lambda_role_arn
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
