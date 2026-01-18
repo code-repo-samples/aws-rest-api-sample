a1e15004009fa4a7a0261ea13f6aeb60edc64e2b
code-repo-samples <54771322+code-repo-samples@users.noreply.github.com>
2026-01-17 20:24:40 -0500

    Update outputs.tf

--- terraform/lambda/outputs.tf
+++ terraform/lambda/outputs.tf
@@ -1,3 +1,4 @@
 output "api_url" {
-  value = aws_apigatewayv2_api.http_api.api_endpoint
+  description = "Base URL of the deployed REST API"
+  value       = aws_apigatewayv2_api.http_api.api_endpoint
 }
